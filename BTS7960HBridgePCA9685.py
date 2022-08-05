eb301411ee0ade1f86a2a44db9724294b678d817
4plus17 <108631209+4plus17@users.noreply.github.com>
2022-08-05 14:40:05 +0200

    Update BTS7960HBridgePCA9685.py

--- BTS7960HBridgePCA9685.py
+++ BTS7960HBridgePCA9685.py
@@ -58,34 +58,22 @@ io.setwarnings(False)
 
 # --- ENDE KONFIGURATION GPIO Adressen ---
 
-# Der Variable leftmotor_in1_pin wird die Varibale IN1 zugeorndet. 
-# Der Variable leftmotor_in2_pin wird die Varibale IN2 zugeorndet. 
-leftmotor_in1_pin = 22
-leftmotor_in2_pin = 23
+# Der Variable motor_in1_pin wird die Varibale IN1 zugeorndet. 
+# Der Variable motor_in2_pin wird die Varibale IN2 zugeorndet. 
+motor_in1_pin = 22
+motor_in2_pin = 23
 # Beide Variablen leftmotor_in1_pin und leftmotor_in2_pin werden als
 # Ausgaenge "OUT" definiert. Mit den beiden Variablen wird die
 # Drehrichtung der Motoren gesteuert.
-io.setup(leftmotor_in1_pin, io.OUT)
-io.setup(leftmotor_in2_pin, io.OUT)
-
-# Der Variable rightmotor_in1_pin wird die Varibale IN1 zugeorndet. 
-# Der Variable rightmotor_in2_pin wird die Varibale IN2 zugeorndet. 
-rightmotor_in1_pin = 13
-rightmotor_in2_pin = 19
-# Beide Variablen rightmotor_in1_pin und rightmotor_in2_pin werden 
-# als Ausgaenge "OUT" definiert. Mit den beiden Variablen wird die
-# Drehrichtung der Motoren gesteuert.
-io.setup(rightmotor_in1_pin, io.OUT)
-io.setup(rightmotor_in2_pin, io.OUT)
+io.setup(motor_in1_pin, io.OUT)
+io.setup(motor_in2_pin, io.OUT)
 
 # Die GPIO Pins des Raspberry Pi werden initial auf False gesetzt.
 # So ist sichger gestellt, dass kein HIGH Signal anliegt und der 
 # Motor Treiber nicht unbeabsichtigt aktiviert wird.
 
-io.output(leftmotor_in1_pin, True)
-io.output(leftmotor_in2_pin, True)
-io.output(rightmotor_in1_pin, True)
-io.output(rightmotor_in2_pin, True)
+io.output(motor_in1_pin, True)
+io.output(motor_in2_pin, True)
 
 # Die Funktion setMotorMode(motor, mode) legt die Drehrichtung der 
 # Motoren fest. Die Funktion verfügt über zwei Eingabevariablen.
@@ -100,32 +88,16 @@ io.output(rightmotor_in2_pin, True)
 # setMotorMode(rightmotor, stopp)   Der rechte Motor ist ausgewaehlt
 #                                   der gestoppt wird.
 
-def setMotorMode(motor, mode):
-   if motor == "leftmotor":
-      if mode == "reverse":
-         io.output(leftmotor_in1_pin, True)
-         io.output(leftmotor_in2_pin, False)
-      elif  mode == "forward":
-         io.output(leftmotor_in1_pin, False)
-         io.output(leftmotor_in2_pin, True)
-      else:
-         io.output(leftmotor_in1_pin, False)
-         io.output(leftmotor_in2_pin, False)
-   elif motor == "rightmotor":
-      if mode == "reverse":
-         io.output(rightmotor_in1_pin, False)
-         io.output(rightmotor_in2_pin, True)      
-      elif  mode == "forward":
-         io.output(rightmotor_in1_pin, True)
-         io.output(rightmotor_in2_pin, False)
-      else:
-         io.output(rightmotor_in1_pin, False)
-         io.output(rightmotor_in2_pin, False)
-   else:
-      io.output(leftmotor_in1_pin, False)
-      io.output(leftmotor_in2_pin, False)
-      io.output(rightmotor_in1_pin, False)
-      io.output(rightmotor_in2_pin, False)
+def setMotorMode(mode):
+	if mode == "reverse":
+		io.output(motor_in1_pin, True)
+		io.output(motor_in2_pin, False)
+	elif  mode == "forward":
+		io.output(motor_in1_pin, False)
+		io.output(motor_in2_pin, True)
+	else:
+		io.output(motor_in1_pin, False)
+		io.output(motor_in2_pin, False)
 
 # Die Funktion setMotorLeft(power) setzt die Geschwindigkeit der 
 # linken Motoren. Die Geschwindigkeit wird als Wert zwischen -1
@@ -141,7 +113,7 @@ def setMotorMode(motor, mode):
 # SetMotorLeft(0.75)  -> der linke Motor dreht mit 75% vorwaerts
 # SetMotorLeft(-0.5)  -> der linke Motor dreht mit 50% rueckwaerts
 # SetMotorLeft(1)     -> der linke Motor dreht mit 100% vorwaerts
-def setMotorLeft(power):
+def setMotor(power):
    int(power)
    if power < 0:
       # Rueckwaertsmodus fuer den linken Motor
@@ -160,61 +132,18 @@ def setMotorLeft(power):
    else:
       # Stoppmodus fuer den linken Motor
       PCA9685_pwm.set_pwm(0, 0, 0)
-      PCA9685_pwm.set_pwm(1, 0, 0)  
-      PCA9685_pwm.set_pwm(2, 0, 0)
-      PCA9685_pwm.set_pwm(3, 0, 0)	
+      PCA9685_pwm.set_pwm(1, 0, 0)
 
-# Die Funktion setMotorRight(power) setzt die Geschwindigkeit der 
-# rechten Motoren. Die Geschwindigkeit wird als Wert zwischen -1 
-# und 1 uebergeben. Bei einem negativen Wert sollen sich die Motoren 
-# rueckwaerts drehen ansonsten vorwaerts. 
-# Anschliessend werden aus den uebergebenen Werten die notwendigen 
-# %-Werte fuer das PWM Signal berechnet.
-
-# Beispiel:
-# Die Geschwindigkeit kann mit +1 (max) und -1 (min) gesetzt werden.
-# Das Beispielt erklaert wie die Geschwindigkeit berechnet wird.
-# setMotorRight(0)     -> der linke Motor dreht mit 0% ist gestoppt
-# setMotorRight(0.75)  -> der linke Motor dreht mit 75% vorwaerts
-# setMotorRight(-0.5)  -> der linke Motor dreht mit 50% rueckwaerts
-# setMotorRight(1)     -> der linke Motor dreht mit 100% vorwaerts   
-   
-def setMotorRight(power):
-   int(power)
-   if power < 0:
-      # Rueckwaertsmodus fuer den rechten Motor
-      pwm = -int(duty_cycle * power)
-      if pwm > duty_cycle:
-         pwm = duty_cycle
-      PCA9685_pwm.set_pwm(2, 0, pwm)
-      PCA9685_pwm.set_pwm(3, 0, 0)  	  
-   elif power > 0:
-      # Vorwaertsmodus fuer den rechten Motor
-      pwm = int(duty_cycle * power)
-      if pwm > duty_cycle:
-         pwm = duty_cycle
-      PCA9685_pwm.set_pwm(2, 0, 0)
-      PCA9685_pwm.set_pwm(3, 0, pwm)  	  
-   else:
-      # Stoppmodus fuer den rechten Motor
-      PCA9685_pwm.set_pwm(0, 0, 0)
-      PCA9685_pwm.set_pwm(1, 0, 0)  
-      PCA9685_pwm.set_pwm(2, 0, 0)
-      PCA9685_pwm.set_pwm(3, 0, 0)	
 	  
 # Die Funktion exit() setzt die Ausgaenge die den Motor Treiber 
 # steuern auf False. So befindet sich der Motor Treiber nach dem 
 # Aufruf derFunktion in einem gesicherten Zustand und die Motoren 
 # sind gestopped.
 def exit():
-   io.output(leftmotor_in1_pin, False)
-   io.output(leftmotor_in2_pin, False)
-   io.output(rightmotor_in1_pin, False)
-   io.output(rightmotor_in2_pin, False)
+   io.output(motor_in1_pin, False)
+   io.output(motor_in2_pin, False)
    io.cleanup()
    # Hier wird das PWM Signal der vier Kanaele auf 0 gesetzt. 
    PCA9685_pwm.set_pwm(0, 0, 0)
    PCA9685_pwm.set_pwm(1, 0, 0)
-   PCA9685_pwm.set_pwm(2, 0, 0)
-   PCA9685_pwm.set_pwm(3, 0, 0)	  
 # Ende des Programmes
